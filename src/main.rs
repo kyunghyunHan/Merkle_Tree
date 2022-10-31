65785a5b39b5d26cbec9bb540df92918331c9466
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-10-31 10:54:37 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use bincode::Error;
+use crypto::{digest::Digest, sha3::Sha3};
+
+pub type Data = Vec<u8>;
+pub type Hash = Vec<u8>;
+
 fn main() {
-    println!("머클트리");
+    let data2 = bincode::serialize("dd").unwrap();
+    let mut hasher = Sha3::sha3_256();
+    hasher.input(&data2);
+
+    println!("{}", hasher.result_str());
 }
 
-fn data() {
-    println!("hello")
+/// 증명 해시를 연결할 때 해시를 넣을 쪽에
+pub struct MerkleTree {
+    pub nodes: Vec<Hash>,
+    pub levels: usize,
 }
